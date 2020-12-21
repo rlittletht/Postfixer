f6a8d206c5c5032042a703515cfc5c0b7eb83843
Rob Little <rlittletht@users.noreply.github.com>
2020-12-20 19:09:49 -0800

    Implement expression parser

--- PostfixText/Expression.cs
+++ PostfixText/Expression.cs
@@ -54,8 +54,17 @@ namespace PostfixText
                 return false;
             }
 
-            public bool ParseNextValueChar(char ch, bool fUnget)
+            /*----------------------------------------------------------------------------
+            	%%Function: ParseNextValueChar
+            	%%Qualified: PostfixText.Parser.Expression.ParseNextValueChar
+            	
+                Parse the next value character, delegating to whatever parser we are
+                currently working with. when done with value 2, we're done
+            ----------------------------------------------------------------------------*/
+            public bool ParseNextValueChar(char ch, out bool fUnget)
             {
+                fUnget = false;
+
                 if (m_state == ParsingState.Value1)
                 {
                     if (m_lhs.ParseNextValueChar(ch, out bool fUngetValueChar))
@@ -69,7 +78,63 @@ namespace PostfixText
                         return true;
                 }
 
-                return false;
+                if (m_state == ParsingState.PreCmpOp)
+                {
+                    if (ComparisonOperator.FAcceptParseStart(ch, out ComparisonOperator cmpOperator))
+                    {
+                        m_comparisonOp = cmpOperator;
+                        m_state = ParsingState.CmpOp;
+                        return true;
+                    }
+
+                    if (!char.IsWhiteSpace(ch))
+                        throw new Exception($"Illegal character {ch} encountered in PreCmpOp");
+
+                    return true;
+                }
+
+                if (m_state == ParsingState.CmpOp)
+                {
+                    if (m_comparisonOp.ParseNextValueChar(ch, out bool fUngetCmpOp))
+                        return true;
+
+                    m_state = ParsingState.PreValue2;
+                    // we're done parsing. if we aren't supposed to unget, then return
+                    if (!fUngetCmpOp)
+                        return true;
+                    
+                    // otherwise fallthrough
+                }
+
+                if (m_state == ParsingState.PreValue2)
+                {
+                    if (Value.FAcceptParseStart(ch, out Value value))
+                    {
+                        m_rhs = value;
+                        m_state = ParsingState.Value2;
+                        return true;
+                    }
+
+                    if (!char.IsWhiteSpace(ch))
+                        throw new Exception($"Illegal character {ch} encountered in PreCmpOp");
+
+                    return true;
+                }
+
+                if (m_state == ParsingState.Value2)
+                {
+                    if (m_rhs.ParseNextValueChar(ch, out bool fUngetValueChar))
+                        return true;
+
+                    m_state = ParsingState.Initial;
+                    // we're done parsing Value1. if we're not supposed to unget
+                    // the character, then return and we'll continue with the
+                    // next char
+                    fUnget = fUngetValueChar;
+                    return false;
+                }
+
+                throw new Exception("unknown state parsing expression");
             }
         }
     }
