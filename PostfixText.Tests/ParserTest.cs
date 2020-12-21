e03c95ae15f7b9f7cfbb5b11732aa0086d8641f6
Rob Little <rlittletht@users.noreply.github.com>
2020-12-21 14:42:57 -0800

    hook up parser to parser components

---
+++ PostfixText.Tests/ParserTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PostfixText.Tests
+{
+    [TestFixture]
+    public class ParserTest
+    {
+        [TestCase("[field]==123", 1)]
+        [TestCase("[field]=={1} ", 1)]
+        [Test]
+        public static void TestParseString(string source, int cClauseExpected)
+        {
+            StringParserClient client = new StringParserClient(source);
+            Parser.Clause clause = Parser.BuildClause(client);
+
+            Assert.AreEqual(cClauseExpected, clause.m_items.Count);
+        }
+
+        [TestCase(new [] { "[foo]","==","123"}, 1)]
+        [TestCase(new[] { "[foo", "]==", "123" }, 1)]
+        [TestCase(new[] { "[foo]", "==", "123" }, 1)]
+        [Test]
+        public static void TestParseArrayString(string[] sourceLines, int cClauseExpected)
+        {
+            StringArrayParserClient client = new StringArrayParserClient(sourceLines);
+            Parser.Clause clause = Parser.BuildClause(client);
+
+            Assert.AreEqual(cClauseExpected, clause.m_items.Count);
+        }
+
+    }
+}
